ade494912932e5d74ca3c8dc5aa20396a14d56ae
Jacob Farnsworth <6502enthusiast@gmail.com>
2017-04-27 11:02:53 -0400

    Initial commit with all files

---
+++ hddiag/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace hddiag
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(173, 210);
+            this.panel1.TabIndex = 1;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(7, 75);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(157, 23);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "Reset DNS Servers";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(7, 46);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(157, 23);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Flush DNS Cache";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(7, 162);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(157, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Disable IPv6 Privacy";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(7, 133);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(157, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Disable Tunnelling";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(7, 104);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Increase rpi_wpa2 Priority";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Troubleshooting Options";
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.progressBar1);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.button6);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(191, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(173, 210);
+            this.panel2.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(3, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(165, 76);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "A report file, named helpdesk.txt, will be created on your desktop. This report c" +
+    "ontains useful diagnostic information and can be provided to your support staff." +
+    "";
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Lime;
+            this.button6.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button6.Location = new System.Drawing.Point(6, 46);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(157, 23);
+            this.button6.TabIndex = 1;
+            this.button6.Text = "Generate Text Report";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Information Collection";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Enabled = false;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(79, 235);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(225, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Command is in progress. Please wait...";
+            this.label4.Visible = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(7, 162);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(156, 23);
+            this.progressBar1.TabIndex = 3;
+            this.progressBar1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(376, 257);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Help Desk Diagnostic Utility";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ hddiag/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+using System.Diagnostics;
+
+namespace hddiag
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //helper function to initialize a Process object
+        //with hidden window attribute
+        private Process beginCommand()
+        {
+            Process proc1 = new Process();
+            proc1.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            proc1.StartInfo.FileName = "cmd.exe";
+            return proc1;
+        }
+
+        //a command just started, disable all buttons
+        //and show the "in progress" text
+        private void DisableAllButtons()
+        {
+            button6.BackColor = Color.Yellow;
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+            button6.Enabled = false;
+            label4.Visible = true;
+            label4.Enabled = true;
+        }
+
+        //this is called after a command has finished
+        //but before the "finished" messagebox is shown
+        private void GreyProgressText()
+        {
+            label4.Enabled = false;
+        }
+
+        //a command just finished, enable all buttons again
+        //and hide the "in progress" text
+        private void EnableAllButtons()
+        {
+            button6.BackColor = Color.Lime;
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+            button6.Enabled = true;
+            label4.Visible = false;
+        }
+
+        //bump up rpi_wpa2 and eduroam priority
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DisableAllButtons();
+
+            Process proc1 = beginCommand();
+            Process proc2 = beginCommand();
+            proc1.StartInfo.Arguments = "/C netsh wlan set profileorder name=\"rpi_wpa2\" interface=\"Wi-Fi\" priority=1";
+            proc2.StartInfo.Arguments = "/C netsh wlan set profileorder name=\"eduroam\" interface=\"Wi-Fi\" priority=2";
+            proc1.Start();
+            proc1.WaitForExit();
+            proc2.Start();
+            proc2.WaitForExit();
+
+            GreyProgressText();
+
+            MessageBox.Show(this, "Command completed successfully.");
+
+            EnableAllButtons();
+        }
+
+        //disable tunnelling
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DisableAllButtons();
+
+            Process proc1 = beginCommand();
+            Process proc2 = beginCommand();
+            Process proc3 = beginCommand();
+            proc1.StartInfo.Arguments = "/C netsh int ipv6 isatap set state disabled";
+            proc2.StartInfo.Arguments = "/C netsh int ipv6 6to4 set state disabled";
+            proc3.StartInfo.Arguments = "/C netsh int teredo set state disabled";
+            proc1.Start();
+            proc1.WaitForExit();
+            proc2.Start();
+            proc2.WaitForExit();
+            proc3.Start();
+            proc3.WaitForExit();
+
+            GreyProgressText();
+
+            MessageBox.Show(this, "Command completed successfully.");
+
+            EnableAllButtons();
+        }
+
+        //disable ipv6 privacy
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DisableAllButtons();
+
+            Process proc1 = beginCommand();
+            proc1.StartInfo.Arguments = "/C netsh interface ipv6 set privacy state=disabled";
+            proc1.Start();
+            proc1.WaitForExit();
+
+            GreyProgressText();
+
+            MessageBox.Show(this, "Command completed successfully.");
+
+            EnableAllButtons();
+        }
+
+        //flush DNS and renew DHCP lease
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DisableAllButtons();
+
+            Process proc1 = beginCommand();
+            Process proc2 = beginCommand();
+            Process proc3 = beginCommand();
+            proc1.StartInfo.Arguments = "/C ipconfig /flushdns";
+            proc2.StartInfo.Arguments = "/C ipconfig /release";
+            proc3.StartInfo.Arguments = "/C ipconfig /renew";
+            proc1.Start();
+            proc1.WaitForExit();
+            proc2.Start();
+            proc2.WaitForExit();
+            proc3.Start();
+            proc3.WaitForExit();
+
+            GreyProgressText();
+
+            MessageBox.Show(this, "Command completed successfully.");
+
+            EnableAllButtons();
+        }
+
+        //set DNS servers to be acquired via DHCP
+        private void button5_Click(object sender, EventArgs e)
+        {
+            DisableAllButtons();
+
+            Process proc1 = beginCommand();
+            proc1.StartInfo.Arguments = "/C netsh interface ip set dns \"Wi-Fi\" dhcp";
+            proc1.Start();
+            proc1.WaitForExit();
+
+            GreyProgressText();
+
+            MessageBox.Show(this, "Command completed successfully.");
+
+            EnableAllButtons();
+        }
+
+        //generate a text report containing diagnostic information
+        private void button6_Click(object sender, EventArgs e)
+        {
+            DisableAllButtons();
+            progressBar1.Visible = true;
+
+            //These commands will be sent to cmd.exe, the pipe
+            //into helpdesk.txt is added automatically
+            string[] commands = new string[]
+            {
+                "date /t",
+                "time /t",
+                "ipconfig /all",
+                "arp -a",
+                "netstat -esr",
+                "netstat -na",
+                "nbtstat -n",
+                "net use",
+                "net user",
+                "net view 127.0.0.1",
+                "net session",
+                "nbtstat -S",
+                "netsh interface ipv6 show route",
+                "netsh interface ipv6 show neighbors",
+                "nslookup www.rpi.edu",
+                "nslookup www.cnn.com",
+                "netsh wlan show profiles",
+                "date /t",
+                "time /t"
+            };
+
+            progressBar1.Maximum = commands.Length;
+            progressBar1.Step = 1;
+
+            Process proc = beginCommand();
+            proc.StartInfo.Arguments = "/C echo HD Diagnostic Utility 0.1a > \"%userprofile%\\Desktop\\helpdesk.txt\"";
+            proc.Start();
+            proc.WaitForExit();
+            
+            foreach(string cmd in commands)
+            {
+                proc = beginCommand();
+                proc.StartInfo.Arguments = "/C " + cmd + " >> \"%userprofile%\\Desktop\\helpdesk.txt\"";
+                proc.Start();
+                proc.WaitForExit();
+
+                progressBar1.PerformStep();
+            }
+
+            GreyProgressText();
+
+            //verify that the file was actually created
+            string desktopFolder = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            if (File.Exists(Path.Combine(desktopFolder, "helpdesk.txt")))
+            {
+                MessageBox.Show(this, "Wrote text report successfully.");
+            }
+            else
+            {
+                MessageBox.Show(this, "Error: Failed to generate text report.\nEnsure you have write permissions for the desktop.");
+            }
+
+            progressBar1.Value = 0;
+            progressBar1.Visible = false;
+
+            EnableAllButtons();
+        }
+    }
+}
